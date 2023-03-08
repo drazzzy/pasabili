8441837131ee60f665453520e16939f7406409fe
drazzy <kimdwightdelacerna@gmail.com>
2023-03-09 04:43:00 +0800

    added constants values and deleted customer home layout

--- app/src/main/java/com/example/pasabili/constants/Constants.java
+++ app/src/main/java/com/example/pasabili/constants/Constants.java
@@ -4,4 +4,6 @@ public class Constants {
     public static final String DB_USER = "Users";
 
     public static final String REALTIME_DATABASE_URL = "https://pasabilifirebase-default-rtdb.asia-southeast1.firebasedatabase.app/";
+
+    public static final String USER_UID = "userUid";
 }
